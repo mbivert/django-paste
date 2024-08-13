27a3986f462cc18dc2a7b87c717f0c1c506f118b
mb <mathieu.bivert@gmail.com>
2024-08-13 11:17:22 +0200

    Adding paste publishing

--- pastesite/paste/urls.py
+++ pastesite/paste/urls.py
@@ -5,5 +5,7 @@ from . import views
 app_name = 'paste'
 
 urlpatterns = [
+	path('',            views.index,   name='index'),
+	path('new/',        views.new,     name='new'),
 	path('p/<str:id>/', views.paste, name='paste'),
 ]
--- pastesite/paste/views.py
+++ pastesite/paste/views.py
@@ -1,7 +1,17 @@
 from django.shortcuts import render, get_object_or_404
 from .models          import Paste
+from django.urls      import reverse
+from django.http      import HttpResponseRedirect
 
 def paste(request, id):
 	p = get_object_or_404(Paste, pk=id)
 	c = {"id" : p.id, "content" : p.content }
 	return render(request, "paste/paste.html", c)
+
+def index(request):
+	return render(request, "paste/index.html")
+
+def new(request):
+	p = Paste(content=request.POST["content"])
+	p.save()
+	return HttpResponseRedirect(reverse("paste:paste", args=(p.id,)))
