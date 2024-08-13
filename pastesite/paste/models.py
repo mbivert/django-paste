d9dca0bfa40ceb3f691aa1ce99ec25e17d3eb72f
mb <mathieu.bivert@gmail.com>
2024-08-13 10:19:14 +0200

    Adding the Paste models and toying around

--- pastesite/paste/models.py
+++ pastesite/paste/models.py
@@ -1,3 +1,22 @@
 from django.db import models
+from typing    import NewType
 
-# Create your models here.
+import string, random
+
+PasteId = NewType('PasteId', str)
+
+def mkpasteid() -> PasteId:
+	return PasteId(''.join(random.choices(string.ascii_letters + string.digits, k=24)))
+
+class Paste(models.Model):
+	id      = PasteId(models.SlugField(
+		primary_key=True, unique=True, editable=False, blank=True
+	))
+	content = models.TextField()
+
+	def save(self, *args, **kwargs):
+		while not self.id:
+			x = mkpasteid()
+			if not Paste.objects.filter(pk=x).exists():
+				self.id = x
+		super().save(*args, **kwargs)
